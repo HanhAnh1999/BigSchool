ad1a347b87cd0309240169e52ced8315cb39960a
Hanh Anh <Administrator@PC213>
2020-06-12 13:44:11 +0700

    Bigschool2.1

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using BigSchool.Models;
+using BigSchool.Views.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Views.COURSE_S
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categoiries = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
